73bc3023e4827d424ef7e8f8c25a44221429ac1a
Ahmed Mosad Fayed <31994329+Ahmed-Fayed@users.noreply.github.com>
2021-03-24 13:38:21 +0200

    Visualizing and Exploring the data

---
+++ Plant_Diseases_Prediction.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 24 13:08:50 2021
+
+@author: Ahmed Fayed
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+
+import os
+import cv2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
